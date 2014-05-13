89f151402302a664590a7ab869e0c642d32d357b
cguiloo <Christophe.Guillou@gmail.com>
2014-05-13 07:44:11 +0100

    adding some files

--- index.js
+++ index.js
@@ -15,15 +15,22 @@ function gulpBomRemover() {
         if (file.isNull()) {
             // Do nothing if no contents
         }
+        //TODO : how can I detect the current encoding?
+
+        //TODO: BOM has different value based on the encoding - shall I map it to a RegEx before processing?
+        //@see http://en.wikipedia.org/wiki/Byte_order_mark
+
+        //TODO : re-use 'enc' encoding ?!
         if (file.isBuffer()) {
-            file.contents = new Buffer(file.contents.replace(BOM_REGEX, ''));
+            file.contents = new Buffer(file.contents.replace(BOM_REGEX, ''), 'utf8');
         }
 
+        //TODO: Transcode the stream here to use the encoding ?!
         if (file.isStream()) {
             file.contents = file.contents.pipe(file.contents.replace(BOM_REGEX, ''));
         }
 
-        //TODO: Writing file with UTF8 encoding
+        //TODO: Transcode/ Return stream with UTF8 encoding
 
         this.push(file);
         return callback();
